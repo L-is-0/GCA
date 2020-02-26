157778c4536360338de3ca2e1d389c281606d323
hazelwww <hazelwang0729@gmail.com>
2020-02-26 15:03:45 +0000

    Added a friend square page

--- lib/pages/square.dart
+++ lib/pages/square.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+export 'package:gca_app/tab//pointTabIndicator.dart';
+import 'package:gca_app/tab//pointTabIndicator.dart';
 
 class Square extends StatefulWidget{
   static const String id = "/square";
@@ -7,15 +9,42 @@ class Square extends StatefulWidget{
   _SquareState createState() => _SquareState();
 }
 
-class _SquareState extends State<Square>{
+class _SquareState extends State<Square>
+  with SingleTickerProviderStateMixin {
+  final tabList = ['Daily List', 'Weekly list'];
+  TabController _tabController;
+
+  @override
+  void initState() {
+  _tabController = TabController(vsync: this, length: tabList.length);
+  super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Square'),
+        title: Text('Friend Square'),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          indicator: PointTabIndicator(
+            position: PointTabIndicatorPosition.bottom,
+            color: Colors.white,
+            insets: EdgeInsets.only(bottom: 6),
+          ),
+          tabs: tabList.map((item) {
+            return Tab(
+              text: item,
+            );
+          }).toList(),
+        ),
       ),
-      body: Center(
-        child: Text('Square Page'),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabList.map((item) {
+          return Center(child: Text(item));
+        }).toList(),
       ),
     );
   }
